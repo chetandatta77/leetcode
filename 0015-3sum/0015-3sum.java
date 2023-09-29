7ff6ee6efae76b7ef322ac93cd052eff28d47516
Chetan Datta <43289457+chetandatta77@users.noreply.github.com>
2023-09-29 13:10:12 +0530

    Time: 31 ms (67.98%), Space: 50.7 MB (52.45%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,34 +3,29 @@ class Solution {
         Set<List<Integer>> set = new HashSet<>();
         Arrays.sort(nums);
 
-        int i = 0;
-        int j = i+1;
-        int k = nums.length-1;
-        while( i< nums.length-2){
+        for(int i=0; i<nums.length; i++) {
+            if(i>0 && nums[i-1]==nums[i]) continue;
 
-            int total = nums[i]+nums[j]+nums[k];
-            if(total<0){
-                while(j<k && nums[j]==nums[j+1]) j++;
-                j+=1;
-            } else if(total>0){ 
-                while(j<k && nums[k-1]==nums[k]) k--;
-                k-=1;
-            } else{
-                List<Integer> triplet = Arrays.asList(nums[i],nums[j],nums[k]);
-                set.add(triplet);
-                j+=1;
-                k-=1;
+            int j = i+1;
+            int k = nums.length-1;
+            while(j<k){
+                int total = nums[i] + nums[j] + nums[k];
+
+                if(total<0) {
+                    j+=1;
+                } else if(total > 0) {
+                    k-=1;
+                } else {
+                    List<Integer> triplet = Arrays.asList(nums[i], nums[j], nums[k]);
+                    set.add(triplet);
+                    j+=1;
+                    k-=1;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
+                }
             }
 
-            if(j>=k){
-                while(i<nums.length-1 && nums[i]==nums[i+1]) i++;
-                i++;
-                j = i+1;
-                k = nums.length-1;
-            } 
-
         }
-
         return new ArrayList<>(set);
     }
 }
