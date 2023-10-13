874fe82521880861a56f920e21edff4d6f601724
Chetan Datta <43289457+chetandatta77@users.noreply.github.com>
2023-10-13 12:45:22 +0530

    Time: 1 ms (99.9%), Space: 48.4 MB (43.28%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,14 +1,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
         int n = nums.length;
-        int votes = 0;
-        int leader = nums[0];
+        int count = 0;
+        int army = nums[0];
 
         for(int member: nums){
-            if(member==leader) votes+=1;
-            else if(votes!=0) votes-=1;
-            else leader = member;
+            if(army==member) count+=1;
+            else if(count!=0) count-=1;
+            else army = member;
         }
-        return leader;
+
+        int actualCount = 0;
+        for(int member:nums){
+            if(army==member) actualCount+=1;
+        }
+
+        if(actualCount<n/2) return -1;
+
+        return army;
     }
 }
